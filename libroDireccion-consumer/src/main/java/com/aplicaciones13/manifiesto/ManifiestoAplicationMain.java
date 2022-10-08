15470c72c58c217ee4ef134829714a782ac7cc08
Omar Vélez <omargo33@gmail.com>
2022-10-08 11:13:36 -0500

    Up [13a-Ov] -> up cambios de pruebas

--- libroDireccion-consumer/src/main/java/com/aplicaciones13/manifiesto/ManifiestoAplicationMain.java
+++ libroDireccion-consumer/src/main/java/com/aplicaciones13/manifiesto/ManifiestoAplicationMain.java
@@ -1,10 +1,5 @@
 package com.aplicaciones13.manifiesto;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
