fd5e0c179d59c6aee3cbf3d2d46baf63175108ba
Omar Velez <omargo33@hotmail.com>
2023-02-05 18:13:00 -0500

    Up [Ov-13a] Primera parte de la fase final del sitema aerocivil.

--- Manifiesto/ViewControllerManifiesto/src/viewManifiesto/manifiestoUsuario/SessionBean.java
+++ Manifiesto/ViewControllerManifiesto/src/viewManifiesto/manifiestoUsuario/SessionBean.java
@@ -1,7 +1,7 @@
-  package viewManifiesto.manifiestoUsuario;
-  
-  import view.utilidades.ADFUtils;
-  
+package viewManifiesto.manifiestoUsuario;
+
+import view.utilidades.ADFUtils;
+
 /**
  * Metodo para anular datos para la pagina de item para las descripciones de datos.
  *
@@ -11,9 +11,22 @@ public class SessionBean {
     public SessionBean() {
         super();
     }
- 
+
+    /**
+     * Metodo para limpiar los datos.
+     */
     public void limpiar() {
-      ADFUtils.setEL("#{sessionScope.aeropuertoDestinoDescripcion}", null);
-      ADFUtils.setEL("#{sessionScope.aeronaveDescripcion}", null);
+        ADFUtils.setEL("#{sessionScope.aeropuertoDestinoDescripcion}", null);
+        ADFUtils.setEL("#{sessionScope.aeronaveDescripcion}", null);
     }
-  }
+
+    /**
+     * Metodo para validar un data ingreso al ejecutar la pantalla de edit varias veces.
+     */
+    public void valiarDataIngreso() {
+        PPRItemFrg pPRItemFrg = (PPRItemFrg) ADFUtils.evaluateEL("#{PPRBackingManifiestoUsuarioItemFrg}");
+        if (pPRItemFrg != null) {
+            pPRItemFrg.limpiarEjecute();
+        }
+    }
+}
